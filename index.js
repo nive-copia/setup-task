c25bf1b1dca070f1dc61026045f17a6e5616b297
Luke Shay <shay.luke17@gmail.com>
2021-03-20 18:46:14 -0500

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const tc = require('@actions/tool-cache');
-const core = require('@actions/core');
-
+const tc = require("@actions/tool-cache");
+const core = require("@actions/core");
 
 async function run() {
-    const taskPath = await tc.downloadTool('https://github.com/go-task/task/releases/download/v3.3.0/task_linux_amd64.tar.gz');
-    const node12ExtractedFolder = await tc.extractTar(taskPath);
-    const cachedPath = await tc.cacheDir(node12ExtractedFolder, "task", "3.3.0");
-    core.addPath(cachedPath);
+  const taskPath = await tc.downloadTool(
+    "https://github.com/go-task/task/releases/download/v3.3.0/task_linux_amd64.tar.gz"
+  );
+  const node12ExtractedFolder = await tc.extractTar(taskPath);
+  const cachedPath = await tc.cacheDir(node12ExtractedFolder, "task", "3.3.0");
+  core.addPath(cachedPath);
 }
 
 run();
