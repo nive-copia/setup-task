7a72d446e363d1f8cf6411bd0400e69d5322a1d2
Luke Shay <shay.luke17@gmail.com>
2021-03-20 18:44:40 -0500

    updated to newer version of task

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const core = require('@actions/core');
 
 
 async function run() {
-    const taskPath = await tc.downloadTool('https://github.com/go-task/task/releases/download/v2.8.1/task_linux_amd64.tar.gz');
+    const taskPath = await tc.downloadTool('https://github.com/go-task/task/releases/download/v3.3.0/task_linux_amd64.tar.gz');
     const node12ExtractedFolder = await tc.extractTar(taskPath);
-    const cachedPath = await tc.cacheDir(node12ExtractedFolder, "task", "2.8.1");
+    const cachedPath = await tc.cacheDir(node12ExtractedFolder, "task", "3.3.0");
     core.addPath(cachedPath);
 }
 
